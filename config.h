f2d3b5cca5571cf224331f2c55a33702cc6c1281
Geoff Siuciak <geoff_siuciak@yahoo.com>
2021-02-04 21:12:44 -0600

    add config

---
+++ config.h
@@ -0,0 +1,26 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+
+#define QUEUE_SIZE 100
+#define MAX_CONNECTIONS 100
+#define DEFAULT_PER_MIN 30
+#define DEFAULT_PER_SESSION 100
+#define DEFAULT_HISTORY_SAVED 250
+#define DEFAULT_CLIENTS_SAVED 500
+#define DEFAULT_LOG_INTERVAL 30
+#define DEFAULT_INIT_THREAD_COUNT 25
+
+#define LOCAL_HOST   
+#define INTERP_MODE
+#define SET_TCP 
+// #define SET_UDP
+#define AUTOLOG
+#define LOGFILE "server_log.txt"
+#define ROOT "/home/xzxthagod/server"
+
+#define INTERP_PROMPT "serv~$ "
+#define BANNED_IP_MSG "u are banned lol\n"
+
+
+#endif // CONFIG_H
