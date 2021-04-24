c22aeeeb793b06fe8738b30d933393f6c1a6f5ad
Geoff Siuciak <geoff_siuciak@yahoo.com>
2021-04-24 15:01:21 -0500

    updates

--- config.h
+++ config.h
@@ -1,16 +1,15 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#define opt_flags "p:tusbd"
 
-#define LOCAL_HOST 1      // explicitly set host IP to 127.0.0.1 
-#define INTERP_MODE 1     // run in interpreter mode - shell and command behavior
-#define AUTOLOG 0         // periodically auto log client db out to file
-#define TERMINAL_OUT 0    // dump msgs to stdout as requests are recieved and filled
-#define LOGFILE "server_log.txt"
+#define LOCAL_HOST      // explicitly set host IP to 127.0.0.1 
+#define INTERP_MODE     // run in interpreter mode - shell and command behavior
+#define AUTOLOG         // periodically auto log client db out to file
+#define TERMINAL_OUT    // dump msgs to stdout as requests are recieved and filled
+#define DEFAULT_LOGFILE "server_log.txt"
 
 #define INTERP_PROMPT "serv~$ "
-#define BANNED_IP_MSG "u are banned lol\n"
-
 #define DEFAULT_LOG_INTERVAL 30
 
 
