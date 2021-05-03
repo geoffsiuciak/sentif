236a755c735e4d5443f2b8321746a2c8d6a155cf
Geoff Siuciak <geoff_siuciak@yahoo.com>
2021-05-02 21:14:34 -0500

    changes

--- src/sentif.cpp
+++ src/sentif.cpp
@@ -1,105 +1,38 @@
-#include "../include/sentif.h"
-  
-int main(int argc, char *argv[]) 
-{
-    int flag;
-	sentif_options_t opt;
-	
-    while((flag = getopt(argc, argv, FLAGS)) != -1) 
-    { 
-        switch(flag) 
-        { 
-            case PORT:
-				opt.port = atoi(optarg);
-				break;
-			case ROOT:
-				opt.root_dir = optarg; 
-            case PROTO_TCP:
-				if (opt.proto == -1) opt.proto = TCP;
-				else init_bail("only 1 proto flag allowed");
-				break;
-            case PROTO_UDP: 
-				if (opt.proto == -1) opt.proto = UDP;
-				else init_bail("only 1 proto flag allowed");
-                break; 
-            case INTERACTIVE: 
-                opt.interact = true; 
-                break; 
-            case BACKGROUND:
-				opt.background = true;
-				break;
-			case LOGGING:
-				opt.logging = true;
-				break;
-			case LOGFILE:
-				opt.logfile = optarg;
-				break;
-			case HELP:
-				show_help(HELP_FILE);
-				break;
-			case HELP_VERBOSE:
-				show_help(HELP_FILE_VERBOSE);
-				break;
-			default:
-				exit(SENTIF_BAIL); 
-        } 
-    } 
-            
-	sentif_init(&opt);
+#include "sentif.h"
 
-    return 0;
+ctx_t* new_opt_ctx()
+{
+    static ctx_t ctx = { 
+		-1, -1, -1, 
+        true, false,
+        nullptr, nullptr
+    };
+    return &ctx;
 }
 
-void sentif_init(const sentif_options_t *op)
+void to_the_moon(const ctx_t *ctx)
 {
-	// 1. check port
-    int sockfd;
-    sockaddr_in serv_addr;
-     
-	if (op->port == -1) init_bail("missing port");
+	printf("sentif running!\n");
+    printf("on host port %i\n", ctx->host);
+    printf("using %s\n", ctx->protocol == TCP ? "TCP" : "UDP");
+    printf("from root %s\n", ctx->root);
 
-    sockfd = socket(AF_INET, op->proto, 0);
-    if (sockfd < 0) { init_bail("failed to open test socket"); }
-  
-    memset((char*)&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(op->port);
+    if (ctx->admin != -1)
+        printf("with admin port %i\n", ctx->admin);
+    else
+        printf("with admin disabled\n");
 
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) {
-        init_bail("port is not avaiable");
-	}
- 
-    close(sockfd);
-	
-	// 2. check protocol
-	if (op->proto == -1) init_bail("missing protocol");
-    
-	// 3. check root directory
-	if (!op->root_dir) {
-		char cwd[256];
-		if (!getcwd(cwd, 256)) {
-			init_bail("failed to get name of cwd");
-		} else {
-			strcpy(op->root_dir, cwd);
-		}
-	}
-	
-	if (!(access(op->root_dir, R_OK) == 0 && access(op->root_dir, W_OK) == 0)) {
-		init_bail("you don't have R/W priviledges for this root");
-	}
-
-	// check/setup logfile
-	if (!op->logfile) { strcpy(op->logfile, DEFAULT_LOGFILE); }
-
-	if (!(access(op->logfile, F_OK) == 0)) {
-		if (!(creat(op->logfile, O_CREAT) > -1))
-			init_bail("failed to init logfile");
-	}
+    sentif::WebServer s(*ctx);
+    s.go();
+    // s.ban("127.0.0.1");
 }
 
-void show_help(const char* file)
+void _(const char* file)
 {
-    char buffer[1024];
+	if (!(access(file, R_OK) == 0)) 
+		bail_("display file not found");    
+	
+	char buffer[1024];
     int bytes;
     int fd = open(file, O_RDONLY);
     
@@ -109,9 +42,8 @@ void show_help(const char* file)
 	exit(SENTIF_PROPER);
 }
 
-void init_bail(const char* msg)
+void bail_(const char* msg)
 {
-    fprintf(stderr, "%s\n", msg);
-	printf("use flags -h or -v for help\n\n");
-    exit(SENTIF_BAIL);
+	fprintf(stderr, "%s\n", msg);
+	exit(SENTIF_BAIL);
 }
