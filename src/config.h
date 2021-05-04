522826f24b91a4afe9ca21594f7db4f9754c7c65
Geoff Siuciak <geoff_siuciak@yahoo.com>
2021-05-03 23:43:41 -0500

    updates

--- src/config.h
+++ src/config.h
@@ -7,15 +7,22 @@
 #define LOGOUT       "./txt/server_log.txt"
 #define ERROUT       "./txt/error_log.txt"
 #define DENY         "./pages/deny.html"
+#define LOGO         "./txt/logo.txt"
 #define ADMIN_PROMPT "~$ "
+
+#define CLIENT             0
+#define ADMIN              1
+#define INVALID_SOCKET    -1
+#define SENTIF_PROPER      0 
+#define SENTIF_ERROR       1
+#define SENTIF_BAIL        2
+#define MAX_LISTEN         100
+#define BUFFER_S           256
+#define BUFFER_L           1026
 
 #define TCP    SOCK_STREAM
 #define UDP    SOCK_DGRAM 
 #define DOMAIN AF_INET
-#define SENTIF_PROPER      0 
-#define SENTIF_ERROR       1
-#define SENTIF_BAIL        2
-#define BUFFER_S           256
-#define BUFFER_L           1026
+
 
 #endif // CONFIG_H
